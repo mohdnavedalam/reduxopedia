1214b09d909635e39e1693fad0fc9d0a9e7d48dc
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-09-17 06:05:08 +0530

    dispatching action from react component

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,9 +1,22 @@
-import { useSelector, UseSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import {increment, decrement} from '../redux/slice/CounterSlice';
 
 const Counter = () => {
     const count = useSelector((state: any) => state.counterStore.count);
+    const dispatch = useDispatch();
     return (
-        <h1>{count}</h1>
+        <div className="mt-2 pt-3 pl-2 text-center">
+            <div className="h4 pb-2">Counter: {count}</div>
+            <div className="row justify-content-center">
+                <div className="p-4 col-12 col-md-6 ">
+                    <div className="border p-4 ">
+                        <button className="btn btn-primary" onClick={() => dispatch(increment())}>Add</button>
+                        &nbsp;&nbsp;
+                        <button className="btn btn-danger" onClick={() => dispatch(decrement())}>Remove</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 export default Counter;
